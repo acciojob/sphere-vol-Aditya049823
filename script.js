fa740e490a8018805b397e288f2f795e897d131e
Aditya Harde <110459473+Aditya049823@users.noreply.github.com>
2023-06-20 17:49:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 function volume_sphere() {
     //Write your code here
-  
+	event.preventDefault();
+	let radius=document.querySelector("#radius");
+	let volumeVal=document.querySelector("#volume");
+	let radiusVal=parseFloat(radius.value);
+	let val=(4/3)*Math.PI*Math.pow(radiusVal,3);
+
+	volumeVal.value=val;
+	
+	
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
